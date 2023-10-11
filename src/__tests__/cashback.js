5aa4ef758d9628fe6051f853d82a062cbe00bfc6
Wayne <meibeat@mail.ru>
2023-10-11 20:54:49 +0300

    eslint fix

--- src/__tests__/cashback.js
+++ src/__tests__/cashback.js
@@ -1,4 +1,4 @@
-import calculateCashback from '../cashback.js';
+import calculateCashback from '../cashback';
 
 test('cashback at sum: 500', () => {
   const result = calculateCashback(500);
@@ -15,9 +15,10 @@ test('cashback at sum: 500', () => {
 
 // const handler = test.each(dataList);
 
-// /** 
+// /**
 //  * %s - означает вставку в качестве строки первого параметра (т.е. 'gold' или 'silver' и т.д.)
-//  * %i - в качестве целого числа (integer) вставляет второй параметр (т.е. 100000 или 10000 и т.д.)
+//  * %i - в качестве целого числа (integer) вставляет второй
+//  параметр (т.е. 100000 или 10000 и т.д.)
 //  *  */
 // handler('testing cashback function with %s status and %i amount', (status, amount, expected) => {
 //   const result = calculateCashback(amount);
@@ -30,8 +31,7 @@ test.each([
   ['silver', 10000, 200],
   ['regular', 1000, 10],
   ['no', 500, 0],
-])
-('testing cashback function with %s status and %i amount', (_, amount, expected) => { // если не используем аргумент (status) можно именовать его как: "_" (нижнее подчеркивание), чтобы это обозначить
+])('testing cashback function with %s status and %i amount', (_, amount, expected) => { // если не используем аргумент (status) можно именовать его как: "_" (нижнее подчеркивание), чтобы это обозначить
   const result = calculateCashback(amount);
   expect(result).toBe(expected);
 });
